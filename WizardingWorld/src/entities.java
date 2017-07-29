9c9b513e4d1dee1caab2defcedbb7f7e0756c0c8
Setare <setashiz@outlook.com>
2017-07-29 11:51:56 +0430

    WizardingWorld Created

---
+++ WizardingWorld/src/entities.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class entities {
+	public class School {
+		private String _name;
+		private int _numOfStudents;
+		private boolean _housed;
+		private ArrayList <String> _houseNames;
+		private ArrayList <String>
+	}
+	public class School(String name, ArrayList <)
+
+}
