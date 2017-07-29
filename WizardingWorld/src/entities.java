cbc830ca38ab790d0532870118e2fe2dc5b01ad1
Setare <setashiz@outlook.com>
2017-07-29 21:50:49 +0430

    school created

--- WizardingWorld/src/entities.java
+++ WizardingWorld/src/entities.java
@@ -7,8 +7,33 @@ public class entities {
 		private int _numOfStudents;
 		private boolean _housed;
 		private ArrayList <String> _houseNames;
-		private ArrayList <String>
-	}
-	public class School(String name, ArrayList <)
+		private ArrayList <String> _courseNames;
+		private Vector <String> _studentsNames;
+		private Vector <String> _proffesorNames;
+		
+ 	public School (String name  , int numOfStudent, boolean housed , ArrayList <String> houseNames , ArrayList <String>  courseNames , Vector<String> studentsNames , Vector<String> proffesorNames ){
+ 		_name= name;
+ 		_houseNames = houseNames;
+ 		_courseNames = courseNames;
+ 		_studentsNames = studentsNames;
+ 		_proffesorNames = proffesorNames;
+ 		if (_studentsNames!= null)
+ 			_numOfStudents = _studentsNames.size();
+ 		else 
+ 			_numOfStudents = 0;
+ 		if (houseNames == null || houseNames.isEmpty())
+ 			_housed = false ;
+ 		else 
+ 			_housed = true ;
+ 		
+ 	}
+ 	
+ 		
+ 		
+ 	}
+ 			
+	
+			
+
 
 }
